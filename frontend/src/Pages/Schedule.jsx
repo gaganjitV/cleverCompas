227db987317230e3cc604c537c7617f4d4c7ca3d
Cristobel <cnavarro-miranda@csus.edu>
2024-11-30 21:21:41 -0500

    same thing

--- frontend/src/Pages/Schedule.jsx
+++ frontend/src/Pages/Schedule.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
+import '../style/schedule.css';
 
 const Schedule = () => {
   return (
-    <Sidebar>
-          {/* Now this us where you will out your html code */}
-      <h1>Schedule</h1>
-      <p>MSchedule</p>
+    <div className="schedule-page">
+    <Sidebar className="sidebar">
+      {/* Sidebar contents */}
     </Sidebar>
-  );
-};
+    <div className= "main-content">
+      {/* Now this us where you will out your html code */}
+      <h1>Schedule</h1>
+      <p>Manage your scheduling needs effectively.</p>
+    </div>
+  </div>
+  )
+}
 
 export default Schedule;
