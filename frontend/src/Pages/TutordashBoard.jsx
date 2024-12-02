a8925dfdafbf8f0e7a9cd2b95405c49449782c03
Gaganjit <83557584+gaganjitV@users.noreply.github.com>
2024-12-01 22:32:42 -0800

    Update TutordashBoard.jsx to connect the socket to live backend

--- frontend/src/Pages/TutordashBoard.jsx
+++ frontend/src/Pages/TutordashBoard.jsx
@@ -4,7 +4,7 @@ import Sidebar from '../components/Sidebar';
 import io from 'socket.io-client';
 
 // Connect to the backend Socket.IO server
-const socket = io('http://localhost:4000');
+const socket = io('https://clevercompas.onrender.com');
 
 const Dashboard = ()=> {
 
