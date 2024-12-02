4acefcacabe19685b96209e14d0fb37f1bd52a69
Gaganjit <83557584+gaganjitV@users.noreply.github.com>
2024-12-01 23:06:19 -0800

    Update login.jsx

--- frontend/src/Pages/login.jsx
+++ frontend/src/Pages/login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../style/login.css';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   
