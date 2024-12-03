f0eaf8910adf57981658bc2de8532f42cf0b62d2
Gaganjit <83557584+gaganjitV@users.noreply.github.com>
2024-12-02 18:53:43 -0800

    Update contact.jsx with rians edits

--- frontend/src/Pages/contact.jsx
+++ frontend/src/Pages/contact.jsx
@@ -12,13 +12,37 @@ const Contact = () => {
       {isLoggedIn && <Sidebar />} {/* Render Sidebar only if logged in */}
     <div className="contact-page">
           {/* Now this us where you will out your html code */}
-      <h1>Contact Us</h1>
-      <p>We'd love to hear from you! Reach out to us through the following channels:</p>
-      <ul>
-        <li>Email: <a href="mailto:support@clevercompass.com">support@clevercompass.com</a></li>
-        <li>Phone: +1 (800) 123-4567</li>
-        <li>Address: 123 Clever Compass Lane, Learning City, Education State</li>
-      </ul>
+      <div className="title-bar">
+        Our Team
+      </div>
+      <p className="subtitle">
+      The Clever Compass team comprises dedicated individuals passionate about improving the tutoring experience:
+      </p>
+      <div className="team-members">
+        <ul>
+          <li>Christobel Navarro-Miranda</li>
+          <li>Mohammad Sumair</li>
+          <li>Vatsal Maniyar</li>
+          <li>Faizan Tariq</li>
+        </ul>
+        <ul>
+          <li>Gaganjit Virdi</li>
+          <li>Ryan Smith</li>
+          <li>Jack Turner</li>
+          <li>Sila Can</li>
+        </ul>
+      </div>
+      <p className="subtitle">
+      We are committed to continuously improving Clever Compass through feedback from our users and staying ahead of industry standards.
+      </p>
+      <div className="contact-info">
+        <p>We'd love to hear from you! Reach out to us through the following channels:</p>
+        <div className="details">
+          <p>Email: <a href="mailto:support@clevercompass.com">support@clevercompass.com</a></p>
+          <p>Phone: +1 (800) 123-4567</p>
+          <p>Address: 123 Clever Compass Lane, Learning City, Education State</p>
+        </div>
+      </div>
     </div>
    </div>
   );
